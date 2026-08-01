1708bba69a60274a3573eb8f8919e9ab7086ffee
Viel0320 <Imlonelywalker@gmail.com>
2026-08-01 17:32:15 +0800

    feat(dev): enable LAN broadcasting in Vite server config
    - Add `server.host: true` in `vite.config.ts` to allow access from local network devices

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  server: {
+    host: true, // 广播到局域网 (0.0.0.0)
+  },
   plugins: [
     react(),
   ],
